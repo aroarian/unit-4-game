36883e0cbe9ba83b3d0cfd5f453ecf35d5338d5d
aroarian <aroarian0@gmail.com>
2018-07-05 15:38:46 -0400

    logic working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,82 +1,121 @@
-var wins = 0;
-var loss = 0;
-var random = 0;
-var crystalNum= 0;
-
-
-
-function resetGame() {
-  random = 0;
-  var userGuess = 0;
-  $(".dispGuess").html(userGuess);
-  startGame();
-}
-
-function startGame() {
+$(document).ready(function() {
   
-  random = 0;
-  userGuess = 0;
+  var random = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+  $(".dispRand").text(random);
+  var wins = 0;
+  $(".dispWins").text(wins);
+  var loss = 0;
+  $(".dispLoss").text(loss);
   var red = Math.floor(Math.random() * 12) + 1;
   var green = Math.floor(Math.random() * 12) + 1;
   var blue = Math.floor(Math.random() * 12) + 1;
   var yellow = Math.floor(Math.random() * 12) + 1;
+  var userGuess = 0;
   $(".dispGuess").html(userGuess);
-  $(".dispWins").text(wins);
-  $(".dispLoss").text(loss);
+  
+ 
+  
+
+  function resetGame() {
+    userGuess = 0;
+    $(".dispGuess").html(userGuess);
+    red = Math.floor(Math.random() * 12) + 1;
+    green = Math.floor(Math.random() * 12) + 1;
+    blue = Math.floor(Math.random() * 12) + 1;
+    yellow = Math.floor(Math.random() * 12) + 1;
+    random = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+    $(".dispRand").text(random);
+
+  };
+
+    $("#red").on("click", function() {
+      userGuess = userGuess + red;
+      $(".dispGuess").text(userGuess);
+
+      if (userGuess == random) {
+        alert("You Win!");
+        wins++;
+        $(".dispWins").text(wins);
+        resetGame();
+        
+
+      } else if (userGuess > random) {
+        alert("You Lose!");
+        loss++;
+        $(".dispLoss").text(loss);
+        resetGame();
+        
+      }
+      console.log("userguess " + userGuess);
+    })
+
+    $("#green").on("click", function() {
+      userGuess = userGuess + green;
+      $(".dispGuess").text(userGuess);
+
+      if (userGuess == random) {
+        alert("You Win!");
+        wins++;
+        $(".dispWins").text(wins);
+        resetGame();
+        
+
+      } else if (userGuess > random) {
+        alert("You Lose!");
+        loss++;
+        $(".dispLoss").text(loss);
+        resetGame();
+        
+      }
+      console.log("userguess " + userGuess);
+    })
+
+    $("#blue").on("click", function() {
+      userGuess = userGuess + blue;
+      $(".dispGuess").text(userGuess);
+
+      if (userGuess == random) {
+        alert("You Win!");
+        wins++;
+        $(".dispWins").text(wins);
+        resetGame();
+        
+
+      } else if (userGuess > random) {
+        alert("You Lose!");
+        loss++;
+        $(".dispLoss").text(loss);
+        resetGame();
+        
+      }
+      console.log("userguess " + userGuess);
+    })
+
+    $("#yellow").on("click", function() {
+      userGuess = userGuess + yellow;
+      $(".dispGuess").text(userGuess);
+
+      if (userGuess == random) {
+        alert("You Win!");
+        wins++;
+        $(".dispWins").text(wins);
+        resetGame();
+        
+
+      } else if (userGuess > random) {
+        alert("You Lose!");
+        loss++;
+        $(".dispLoss").text(loss);
+        resetGame();
+        
+      }
+      console.log("userguess " + userGuess);
+    })
+
+    
+    console.log("random " + random);
+    console.log(red);
+  
 
  
-  random = Math.floor(Math.random() * (120 - 19 + 1) + 19);
-  parseInt(random);
-
-  $(".dispRand").html(random);
-
-  $("#red").on("click", function() {
-    userGuess = userGuess + red;
-    parseInt(userGuess);
-    $(".dispGuess").html(userGuess);
-    console.log("userguess " + userGuess);
-  })
-
-  $("#green").on("click", function() {
-    userGuess = userGuess + green;
-    parseInt(userGuess);
-    $(".dispGuess").html(userGuess);
-    console.log(green);
-    console.log("userguess " + userGuess);
-  })
-
-  $("#blue").on("click", function() {
-    userGuess = userGuess + blue;
-    parseInt(userGuess);
-    $(".dispGuess").html(userGuess);
-    console.log(blue);
-    console.log("userguess " + userGuess);
-  })
-
-  $("#yellow").on("click", function() {
-    userGuess = userGuess + yellow;
-    parseInt(userGuess);
-    $(".dispGuess").html(userGuess);
-    console.log(yellow);
-    console.log("userguess " + userGuess);
-  })
-    if (userGuess == random) {
-      alert("You Win!");
-      win++;
-      resetGame();
-    }
-
-    
-    if (userGuess > random) {
-      alert("You Lose!");
-      userGuess = 0;
-      $(".dispGuess").html(userGuess);
-      loss++;
-      resetGame();
-    }
-    
-  console.log("userguess " + userGuess);
-  console.log("random " + random);
-}
-
-startGame();
+});
